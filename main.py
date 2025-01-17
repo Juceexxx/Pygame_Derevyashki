17079103a1662198da57b6f9938e878ddd5e1564
SandS <Sandstime8@gmail.com>
2025-01-17 22:26:28 +0500

    переработал загрузку изображени, надо чтобы они были в папке Data

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ pygame.init()
 # Настройки экрана
 # Получаем текущее разрешение экрана
 infoObject = pygame.display.Info()
-WIDTH, HEIGHT = 800, 600
+current_resolution = WIDTH, HEIGHT = 800, 600
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Древесные Рыцари")
 
@@ -25,7 +25,7 @@ def load_image(name):
     fullname = os.path.join('Data', name)
     # если файл не существует, то выходим
     if not os.path.isfile(fullname):
-        print(f"Файл с изображением '{fullname}' не найден")
+        print(f'Файл с изображением "{fullname}" не найден')
         sys.exit()
     image = pygame.image.load(fullname)
     return image
@@ -35,6 +35,10 @@ LPS_image = load_image('LPS.png')
 LPS_image = pygame.transform.scale(LPS_image, (100, 100))
 button_image = load_image('Button.png')
 button_image = pygame.transform.scale(button_image, (300, 70))
+standart_button_image = load_image('Sandart_Button.png')
+standart_button_image = pygame.transform.scale(standart_button_image, (300, 70))
+back_button_image = load_image('Back_Button.png')
+back_button_image = pygame.transform.scale(back_button_image, (240, 60))
 new_button_image = load_image('New_Button.png')
 new_button_image = pygame.transform.scale(new_button_image, (300, 70))
 settings_button_image = load_image('Settings_Button.png')
@@ -43,11 +47,28 @@ exit_button_image = load_image('Exit_Button.png')
 exit_button_image = pygame.transform.scale(exit_button_image, (300, 70))
 next_button_image = load_image('Next_Button.png')
 next_button_image = pygame.transform.scale(next_button_image, (300, 70))
+grass_without_wind_image = load_image('Grass_Without_Wind.png')
+# grass_without_wind_image
+grass_with_wind = load_image('Grass_With_Wind.png')
+# grass_with_wind
+grass_with_strong_wind = load_image('Grass_With_Strong_Wind.png')
+# grass_with_strong_wind
 background_image = load_image('background.png')
-background_image = pygame.transform.scale(background_image, (WIDTH, HEIGHT))
+background_image = pygame.transform.scale(background_image, current_resolution)
+
+
+def screen_resolution():
+    if current_resolution == (800, 600):
+        screen.blit(pygame.transform.scale(background_image, current_resolution), (0, 0))
+    elif current_resolution == (1024, 768):
+        screen.blit(pygame.transform.scale(background_image, current_resolution), (0, 0))
+    elif current_resolution == (1280, 720):
+        screen.blit(pygame.transform.scale(background_image, current_resolution), (0, 0))
+    elif current_resolution == (1920, 1080):
+        screen.blit(pygame.transform.scale(background_image, current_resolution), (0, 0))
+
 
 # Настройка экрана в полноэкранном режиме
-current_resolution = (WIDTH, HEIGHT)
 screen = pygame.display.set_mode(current_resolution)
 pygame.display.set_caption('Древесные Рыцари')
 
@@ -81,49 +102,51 @@ def main_menu():
                 sys.exit()
 
         # Отображение фонового изображения
-        screen.blit(background_image, (0, 0))
+        screen_resolution()
 
-        # Фон кнопок и заголовкае
+        pygame.draw.line(screen, 'white', (WIDTH // 2, 0), (WIDTH // 2, HEIGHT), width=5)
+
+        # Фон кнопок и заголовка
         surf = pygame.Surface((325, 400))
         surf.fill(DARK_GRAY)
         surf.set_alpha(200)
-        screen.blit(surf, (262, 90))
+        screen.blit(surf, (WIDTH // 2 - 162, HEIGHT // 6.67))
 
         # Отображение заголовка
-        draw_text('Деревяшки', font, WHITE, screen, 275, 100)
+        draw_text('Деревяшки', font, WHITE, screen, WIDTH // 2 - 150, HEIGHT // 6)
 
         # Создание кнопок
-        start_button = screen.blit(button_image, (275, 183))
-        continue_button = screen.blit(button_image, (275, 253))
-        settings_button = screen.blit(button_image, (275, 323))
-        exit_button = screen.blit(button_image, (275, 393))
-        draw_text('Новая игра', small_font, DIRTY_WHITE, screen, 315, 193)
-        draw_text('Продолжить', small_font, DIRTY_WHITE, screen, 307, 263)
-        draw_text('Настройки', small_font, DIRTY_WHITE, screen, 320, 333)
-        draw_text('Выход', small_font, DIRTY_WHITE, screen, 340, 403)
+        start_button = screen.blit(button_image, (WIDTH // 2 - 150, HEIGHT // 3.279))
+        continue_button = screen.blit(button_image, (WIDTH // 2 - 150, HEIGHT // 3.279 + 70))
+        settings_button = screen.blit(button_image, (WIDTH // 2 - 150, HEIGHT // 3.279 + 140))
+        exit_button = screen.blit(button_image, (WIDTH // 2 - 150, HEIGHT // 3.279 + 210))
+        draw_text('Новая игра', small_font, DIRTY_WHITE, screen, start_button.x + 40, start_button.y + 13)
+        draw_text('Продолжить', small_font, DIRTY_WHITE, screen, continue_button.x + 32, continue_button.y + 13)
+        draw_text('Настройки', small_font, DIRTY_WHITE, screen, settings_button.x + 45, settings_button.y + 13)
+        draw_text('Выход', small_font, DIRTY_WHITE, screen, exit_button.x + 65, exit_button.y + 13)
 
         # Обработка нажатий кнопок и их изменение
         mouse_pos = pygame.mouse.get_pos()
         mouse_click = pygame.mouse.get_pressed()
 
         if start_button.collidepoint(mouse_pos):
-            screen.blit(new_button_image, (275, 183))
-            draw_text('Новая игра', small_font, DIRTY_WHITE, screen, 315, 193)
+            screen.blit(new_button_image, (WIDTH // 2 - 150, HEIGHT // 3.279))
+            draw_text('Новая игра', small_font, DIRTY_WHITE, screen, start_button.x + 40, start_button.y + 13)
             if mouse_click[0]:
                 game_loop()  # Вызов функции игры
         elif continue_button.collidepoint(mouse_pos):
-            screen.blit(next_button_image, (275, 253))
-            draw_text('Продолжить', small_font, DIRTY_WHITE, screen, 307, 263)
+            screen.blit(next_button_image, (WIDTH // 2 - 150, HEIGHT // 3.279 + 70))
+            draw_text('Продолжить', small_font, DIRTY_WHITE, screen, continue_button.x + 32, continue_button.y + 13)
             if mouse_click[0]:
                 continue_game()  # Вызов функции продолжения игры
         elif settings_button.collidepoint(mouse_pos):
-            screen.blit(settings_button_image, (275, 323))
-            draw_text('Настройки', small_font, DIRTY_WHITE, screen, 320, 333)
+            screen.blit(settings_button_image, (WIDTH // 2 - 150, HEIGHT // 3.279 + 140))
+            draw_text('Настройки', small_font, DIRTY_WHITE, screen, settings_button.x + 45, settings_button.y + 13)
             if mouse_click[0]:
                 settings_menu()  # Вызов функции настроек
         elif exit_button.collidepoint(mouse_pos):
-            screen.blit(exit_button_image, (275, 393))
-            draw_text('Выход', small_font, DIRTY_WHITE, screen, 340, 403)
+            screen.blit(exit_button_image, (WIDTH // 2 - 150, HEIGHT // 3.279 + 210))
+            draw_text('Выход', small_font, DIRTY_WHITE, screen, exit_button.x + 65, exit_button.y + 13)
             if mouse_click[0]:
                 if WIDTH // 2 - 150 <= mouse_pos[0] <= WIDTH // 2 + 150:
                     if HEIGHT // 2 + 20 <= mouse_pos[1] <= HEIGHT // 2 + 60:
@@ -217,10 +240,10 @@ def continue_game():
 
 # Функция для настроек
 def settings_menu():
-    global current_resolution, screen, full_screen
+    global current_resolution, screen, full_screen, WIDTH, HEIGHT
 
-    resolutions = [(800, 600), (1024, 768), (1280, 720)]
-    difficulty_options = ['Легкий', 'Средний', 'Сложный']
+    resolutions = [(1920, 1080), (1280, 720), (1024, 768), (800, 600)]
+    difficulty_options = ['Большой детина', 'Идеальный баланс', 'Суровая реальность']
 
     while True:
         for event in pygame.event.get():
@@ -229,47 +252,47 @@ def settings_menu():
                 sys.exit()
 
         # Отображение фона меню настроек
-        screen.blit(background_image, (0, 0))
-
+        screen_resolution()
         # Заголовок настроек
-        draw_text('Настройки', font, WHITE, screen, current_resolution[0] // 2 - 150, 50)
+        draw_text('Настройки', font, WHITE, screen, WIDTH // 2 - 150, 50)
 
-        # Уровень сложности
+        # Уровень сложности ToDo перепешу
         draw_text('Уровень сложности:', small_font, WHITE, screen, current_resolution[0] // 2 - 150, 150)
+        draw_text('Разрешение экрана:', small_font, WHITE, screen, current_resolution[0] // 2 - 150, 350)
+
         for i, option in enumerate(difficulty_options):
             text_color = WHITE if option != difficulty_level else DIRTY_WHITE
             draw_text(option, small_font, text_color, screen, current_resolution[0] // 2 - 150, 200 + i * 40)
-            draw_text('Разрешение экрана:', small_font, WHITE, screen, current_resolution[0] // 2 - 150, 350)
-            for j, res in enumerate(resolutions):
-                res_text = f'{res[0]}x{res[1]}'
-                text_color = WHITE if res != current_resolution else DIRTY_WHITE
-                draw_text(res_text, small_font, text_color, screen, current_resolution[0] // 2 - 150, 400 + j * 40)
+        for j, res in enumerate(resolutions):
+            res_text = f'{res[0]}x{res[1]}'
+            text_color = WHITE if res != current_resolution else DIRTY_WHITE
+            draw_text(res_text, small_font, text_color, screen, current_resolution[0] // 2 - 150, 400 + j * 40)
 
-            # Кнопка "Входной/Полноэкранный режим"
-            mode_text = "Переключить на полноэкранный режим" if not full_screen else "Переключить на оконный режим"
-            draw_text(mode_text, small_font, WHITE, screen, current_resolution[0] // 2 - 850, HEIGHT - 1000)
+        # Кнопка "Входной/Полноэкранный режим"
+        mode_text = "Переключить на полноэкранный режим" if not full_screen else "Переключить на оконный режим"
+        draw_text(mode_text, small_font, WHITE, screen, current_resolution[0] // 2 - 850, HEIGHT - 1000)
 
-            back_button = pygame.Rect(current_resolution[0] // 2 - 150, HEIGHT - 550, 100, 30)
-            pygame.draw.rect(screen, WHITE, back_button)
-            draw_text('Назад', small_font, DARK_GRAY, screen, back_button.x + 10, back_button.y)
+        back_button = screen.blit(back_button_image, (10, HEIGHT - 70))
+        draw_text('Назад', small_font, DIRTY_WHITE, screen, back_button.x + 100, back_button.y + 5)
 
-            # Обработка нажатий
-            mouse_pos = pygame.mouse.get_pos()
-            mouse_click = pygame.mouse.get_pressed()
+        # Обработка нажатий
+        mouse_pos = pygame.mouse.get_pos()
+        mouse_click = pygame.mouse.get_pressed()
 
-            for k, res in enumerate(resolutions):
-                res_rect = pygame.Rect(current_resolution[0] // 2 - 150, 400 + k * 40 - 5, 100, 30)
-                if res_rect.collidepoint(mouse_pos) and mouse_click[0]:
-                    current_resolution = res
+        for k, res in enumerate(resolutions):
+            res_rect = pygame.Rect(current_resolution[0] // 2 - 150, 400 + k * 40 - 5, 100, 30)
+            if res_rect.collidepoint(mouse_pos) and mouse_click[0]:
+                current_resolution = res
+                WIDTH, HEIGHT = current_resolution
+                screen = pygame.display.set_mode(current_resolution)
+                screen.blit(background_image, (0, 0))
 
-                    screen = pygame.display.set_mode(current_resolution)
+        if back_button.collidepoint(mouse_pos):
+            if mouse_click[0]:
+                return
 
-            if back_button.collidepoint(mouse_pos):
-                if mouse_click[0]:
-                    return
-
-            # Обновление экрана
-            pygame.display.flip()
+        # Обновление экрана
+        pygame.display.flip()
 
 
 # Запуск
